a46020a21b30883087336e5ab3a9258d3d6cdd00
one-green-bird <93067923+one-green-bird@users.noreply.github.com>
2022-07-26 22:14:45 +0800

    change the code for DistributedDataparallel

--- segm/utils/torch.py
+++ segm/utils/torch.py
@@ -22,11 +22,12 @@ def set_gpu_mode(mode):
     global distributed
     global dist_rank
     global world_size
-    gpu_id = int(os.environ.get("SLURM_LOCALID", 0))
+    
+    gpu_id = [0,1,2,3]
     dist_rank = int(os.environ.get("SLURM_PROCID", 0))
-    world_size = int(os.environ.get("SLURM_NTASKS", 1))
+    world_size = int(os.environ.get("SLURM_NTASKS", 4))
 
     distributed = world_size > 1
     use_gpu = mode
-    device = torch.device(f"cuda:{gpu_id}" if use_gpu else "cpu")
+    dedvice = torch.device("cuda" if use_qpu else "cpu")
     torch.backends.cudnn.benchmark = True
